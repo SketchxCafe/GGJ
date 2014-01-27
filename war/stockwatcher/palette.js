51cd00596023d62e8d7d9d1ec921de151f4edc6b
SketchxCafe <tweety_808@hotmail.com>
2014-01-26 16:59:30 -0800

    can change most ink and line size

---
+++ war/stockwatcher/palette.js
@@ -0,0 +1,31 @@
+var getTheCanvas = document.getElementById("coolCanvas");
+var canvas = getTheCanvas.getContext('2d');
+
+getCanvas:function() {
+	
+}
+//canvas
+colorChange:function(id) {
+	var getTheCanvas = document.getElementById("coolCanvas");
+	var canvas = getTheCanvas.getContext('2d');
+	canvas.strokeStyle=id;
+}
+
+penSize: function(id) {
+	var getTheCanvas = document.getElementById("coolCanvas");
+	var canvas = getTheCanvas.getContext('2d');
+	canvas.lineWidth=id;
+}
+
+finish:function() {
+	
+}
+
+quit:function() {
+	
+}
+
+trash:function() {
+	//canvas	
+	canvas.
+}
